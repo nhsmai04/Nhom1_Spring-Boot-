d939fa3ba8ac6301655413ede09f8799f70e39e2
quynhanh <bangnguyenquynhanh@gmail.com>
2025-08-05 00:05:49 +0700

    update pom, security, add yml.ex

--- TodoList/src/main/java/com/example/todolist/security/SecurityConfig.java
+++ TodoList/src/main/java/com/example/todolist/security/SecurityConfig.java
@@ -1,19 +1,33 @@
 package com.example.todolist.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/todos/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/todos", true)
+                        .permitAll()
+                )
+
+                .logout(logout -> logout.permitAll());
         return http.build();
     }
 }
-
